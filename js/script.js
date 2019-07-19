2a90bf29cb6f530688fe32519e24e829e5ccb730
JJ <joaoart42@gmail.com>
2019-07-18 21:42:17 -0300

    Atualização do menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-function myFunction(){
-    alert('B');
-}
+$( "#socorro" ).click(function() {
+    console.log('a');
+  });
+
+  $( "#content" ).click(function() {
+    console.log('a');
+  });
+  $( "#nav" ).click(function() {
+    console.log('a');
+  });
+
+
+
+$( "#banner" ).one( "click", function() {
+    $( this ).css( "width", "200" );
+  });
