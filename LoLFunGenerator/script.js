ba79760dc7a8f24aa6ca50c6e63e079a3dba8ff7
JJ <joaoart42@gmail.com>
2020-02-11 00:00:44 -0200

    Atualização do Gerador de Estratégia
    O Resto do site foi apagado pq tava ruim
    
    Gerador de Builds (V1.0.0):
    -Atualização da interface
    -Cores nos feitiços
    -Atualização das cores das runas
    -Atualização do texto do botão
    -Criação do bonus em váriavel (ainda não funciona a randomização)
    -Agora também gera as Builds

--- LoLFunGenerator/script.js
+++ LoLFunGenerator/script.js
@@ -37,22 +37,58 @@ $( document ).ready(function() {
 
     var feiticos = ["Curar", "Fantasma", "Barreira", "Exaustão", "Flash", "Teleporte", "Golpear", "Incendiar", "Purificar"]
 
+    var builds = ["Armadura", "Vida", "Reistência Mágica", "Crítico", "AP", "Letalidade", "Velocidade de Movimento", "Bota + 5x outro item", "Bota + 5 itens ativaveis"]
+
+    function corFeitico(feitico){
+        switch (feitico){
+            case 0:
+                return "82ec53";
+            case 1:
+                return "27d1e0";
+            case 2:
+                return "c9c487";
+            case 3:
+                return "baa345";
+            case 4:
+                return "ede62f";
+            case 5:
+                return "9639b1";
+            case 6:
+                return "efc136";
+            case 7:
+                return "ef6f36";
+            case 8:
+                return "4dcfc1";
+            default:
+                return "293542";
+        }
+    }
+
     function gerar(){
         // Randomização
         var campeao = Math.trunc(Math.random() * ((campeoes.length - 1) - 0) + 0);
         var runa = Math.trunc(Math.random() * ((runas.length - 1) - 0) + 0);
         var feitico1 = Math.trunc(Math.random() * ((feiticos.length - 1) - 0) + 0);
-        var feitico2 = Math.trunc(Math.random() * ((feiticos.length - 1) - 0) + 0);
+        var feitico2;
+        do{
+            feitico2 = Math.trunc(Math.random() * ((feiticos.length - 1) - 0) + 0);
+        } while (feitico2 == feitico1)
+        build = Math.trunc(Math.random() * ((builds.length - 1) - 0) + 0);
         var corRuna;
+        var corFeitico1 = corFeitico(feitico1);
+        var corFeitico2 = corFeitico(feitico2);
 
-        if (runa == 0 || runa == 1 || runa == 2 || runa == 3) { corRuna = "c8aa6e" }
-        if (runa == 4 || runa == 5 || runa == 6 || runa == 7){ corRuna = "d44242" }
-        if (runa == 8 || runa == 9 || runa == 10){ corRuna = "9faafc" }
-        if (runa == 11 || runa == 12 || runa == 13){ corRuna = "a1d586" }
-        if (runa == 14 || runa == 15 || runa == 16){ corRuna = "49aab9" }
+        var bonus = "";
+
+        if (runa == 0 || runa == 1 || runa == 2 || runa == 3) { corRuna = "bca87b" }
+        if (runa == 4 || runa == 5 || runa == 6 || runa == 7){ corRuna = "d74545" }
+        if (runa == 8 || runa == 9 || runa == 10){ corRuna = "7e87f6" }
+        if (runa == 11 || runa == 12 || runa == 13){ corRuna = "a2d08a" }
+        if (runa == 14 || runa == 15 || runa == 16){ corRuna = "49aebb" }
+
         // Exibição em tela
-    
-        $("#champ").html(campeoes[campeao] + ' de <span style = "color: #' + corRuna + '">' + runas[runa] + '</span>, <span>' + feiticos[feitico1] + '</span> e <span>' + feiticos[feitico2] + '</span>, buildando BUILD. [e bônus.]')
+        $("#champ").html(campeoes[campeao] + ' de <span style = "color: #' + corRuna + '">' + runas[runa] + '</span>,<br> <span style ="color: #' + corFeitico1 + '">' + feiticos[feitico1] +
+        '</span> e <span style ="color: #' + corFeitico2 + '">' + feiticos[feitico2] + '</span>,<br> buildando <span style="color:#D00">' + builds[build] + '</span>. <br>' + bonus)
     }
 
     $("button").click(function(){
